491d8a68ee70481e5d0cc8b4c0646dd2703b1870
gan jie fong <jiefonggan@gmail.com>
2023-11-09 14:12:26 +0800

    first commit

---
+++ layout/inventory.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    
+    <input type="checkbox" id="sidebar-toggle">
+    <div class="sidebar">
+        <div class="sidebar-header">
+            <h3 class="brand">
+                <span class="ti-unlink"></span> 
+                <span>easywire</span>
+            </h3> 
+            <label for="sidebar-toggle" class="ti-menu-alt"></label>
+        </div>
+        
+        <div class="sidebar-menu">
+        <ul>
+                <li>
+                    <a href="../index.html">
+                        <span class="ti-home"></span>
+                        <span>Home</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="inventory.php">
+                        <span class="ti-package"></span>
+                        <span>Inventory</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span class="ti-shopping-cart"></span>
+                        <span>Order</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span class="ti-truck"></span>
+                        <span>Warehouse</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span class="ti-agenda"></span>
+                        <span>Supplier</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span class="ti-pie-chart"></span>
+                        <span>Report</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span class="ti-settings"></span>
+                        <span>Setting</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    
+    
+    <div class="main-content">
+        
+        <header>
+            <div class="directory-tag">
+                <p>Inventory</p>
+            </div>
+
+            <div class="social-icons">
+                <div></div>
+                <span>username</span>
+            </div>
+        </header>
+        
+        <main>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Project</th>
+                        <th>Start Date</th>
+                        <th>End Date</th>
+                        <th>Team</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>App Development</td>
+                        <td>15 Aug, 2020</td>
+                        <td>22 Aug, 2020</td>
+                        <td class="td-team">
+                            <div class="img-1"></div>
+                            <div class="img-2"></div>
+                            <div class="img-3"></div>
+                        </td>
+                        <td>
+                            <span class="badge success">Success</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Logo Design</td>
+                        <td>15 Aug, 2020</td>
+                        <td>22 Aug, 2020</td>
+                        <td class="td-team">
+                            <div class="img-1"></div>
+                            <div class="img-2"></div>
+                            <div class="img-3"></div>
+                        </td>
+                        <td>
+                            <span class="badge warning">Processing</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Server setup</td>
+                        <td>15 Aug, 2020</td>
+                        <td>22 Aug, 2020</td>
+                        <td class="td-team">
+                            <div class="img-1"></div>
+                            <div class="img-2"></div>
+                            <div class="img-3"></div>
+                        </td>
+                        <td>
+                            <span class="badge success">Success</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Front-end Design</td>
+                        <td>15 Aug, 2020</td>
+                        <td>22 Aug, 2020</td>
+                        <td class="td-team">
+                            <div class="img-1"></div>
+                            <div class="img-2"></div>
+                            <div class="img-3"></div>
+                        </td>
+                        <td>
+                            <span class="badge warning">Processing</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Web Development</td>
+                        <td>15 Aug, 2020</td>
+                        <td>22 Aug, 2020</td>
+                        <td class="td-team">
+                            <div class="img-1"></div>
+                            <div class="img-2"></div>
+                            <div class="img-3"></div>
+                        </td>
+                        <td>
+                            <span class="badge success">Success</span>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </main>
+        
+    </div>
+    
+</body>
+</html>
