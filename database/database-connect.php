4ba793bc202587ee426c33562293fa10e59e3261
gan jie fong <jiefonggan@gmail.com>
2023-12-02 01:22:43 +0800

    tiny change

--- database/database-connect.php
+++ database/database-connect.php
@@ -14,14 +14,14 @@ if (isset($_SESSION['companyname'])) {
 }
 
 // Replace these values with your Azure SQL Database connection details
-$serverName = "your-server-name.database.windows.net";
+$serverName = "tcp:allhereserver.database.windows.net,1433";
 $database = $companyname;
 $uid = "sqladmin";
 $pwd = "#Allhere";
 
 // Check the connection
 try {
-    $conn = new PDO("sqlsrv:Server=$serverName;Database=$database;Encrypt=true;TrustServerCertificate=false", $uid, $pwd);
+    $conn = new PDO("sqlsrv:server=$serverName;Database=$database", $uid, $pwd);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
