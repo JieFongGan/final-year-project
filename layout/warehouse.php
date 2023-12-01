f9ed8ab28ea465fd70d145ea6cc2be92792b1264
gan jie fong <jiefonggan@gmail.com>
2023-12-02 03:22:34 +0800

    warehouse

--- layout/warehouse.php
+++ layout/warehouse.php
@@ -1,26 +1,22 @@
 <?php
-$pageTitle = "Warehouse";
-include("../database/database-connect.php");
+$pageTitle = "Warehouses";
+include '../database/database-connect.php';
 include '../contain/header.php';
 
+// Prepare the SQL statement
+$sql = "SELECT * FROM Warehouse";
+$stmt = $conn->prepare($sql);
+
+// Execute the statement
+$stmt->execute();
+
+// Fetch the results
+$warehouses = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 // Pagination
-$itemsPerPage = isset($_GET['itemsPerPage']) ? (int) $_GET['itemsPerPage'] : 10;
-
-// Fetch total number of warehouses
-$sqlTotalWarehouses = "SELECT COUNT(*) FROM Warehouse";
-$resultTotalWarehouses = $conn->query($sqlTotalWarehouses);
-
-// Check if the query was successful
-if ($resultTotalWarehouses) {
-    $totalWarehouses = $resultTotalWarehouses->fetch_row()[0];
-
-    // Calculate total pages and handle division by zero
-    $totalPages = $totalWarehouses > 0 ? ceil($totalWarehouses / $itemsPerPage) : 1;
-} else {
-    // Handle query error
-    echo "Error fetching total warehouses: " . $conn->error;
-    exit();
-}
+$itemsPerPage = isset($_GET['itemsPerPage']) ? (int)$_GET['itemsPerPage'] : 10;
+$totalItems = count($warehouses);
+$totalPages = ceil($totalItems / $itemsPerPage);
 
 // Get the current page from the URL, default to 1 if not set
 $current_page = isset($_GET['page']) ? $_GET['page'] : 1;
@@ -29,54 +25,43 @@ $current_page = max(1, min($totalPages, $current_page));
 // Calculate the offset
 $offset = ($current_page - 1) * $itemsPerPage;
 
-// Fetch a subset of warehouses based on the offset and items per page
-$sqlSubsetWarehouses = "SELECT * FROM Warehouse LIMIT $itemsPerPage OFFSET $offset";
-$resultSubsetWarehouses = $conn->query($sqlSubsetWarehouses);
+$subsetWarehouses = array_slice($warehouses, $offset, $itemsPerPage);
 
-// Check if the query was successful
-if ($resultSubsetWarehouses) {
-    $subsetWarehouses = $resultSubsetWarehouses->fetch_all();
-} else {
-    // Handle query error
-    echo "Error fetching subset of warehouses: " . $conn->error;
-    exit();
-}
-
-if (isset($_POST['Cnew'])) {
+if (isset($_POST['Wnew'])) {
     header("Location: warehouse-new.php");
     exit();
 }
 
 if (isset($_POST['deleteWarehouse'])) {
-    $warehouseIDToDelete = mysqli_real_escape_string($conn, $_POST['deleteWarehouse']);
+    // Using prepared statement to prevent SQL injection
+    $warehouseIDToDelete = $_POST['deleteWarehouse'];
+    $deleteSql = "DELETE FROM Warehouse WHERE WarehouseID = ?";
+    $stmtDeleteWarehouse = $conn->prepare($deleteSql);
+    $stmtDeleteWarehouse->bindParam(1, $warehouseIDToDelete, PDO::PARAM_INT);
+    $stmtDeleteWarehouse->execute();
 
-    $deleteSql = "DELETE FROM Warehouse WHERE WarehouseID = '$warehouseIDToDelete'";
-    $deleteResult = $conn->query($deleteSql);
-
-    if ($deleteResult) {
+    if ($stmtDeleteWarehouse->rowCount() > 0) {
         header("Location: warehouse.php");
         exit();
     } else {
-        echo "Error deleting warehouse: " . $conn->error;
+        echo "Error: " . $stmtDeleteWarehouse->errorInfo()[2];
     }
+
+    $stmtDeleteWarehouse->closeCursor();
 }
 
-$conn->close();
 ?>
 
 <div class="main-content">
-
     <?php
-    $pathtitle = "Warehouse";
+    $pathtitle = "Warehouses";
     include '../contain/horizontal-bar.php';
     ?>
 
     <main>
         <div class="button-and-search">
             <form method="POST">
-            <?php if ($userrole !== 'Manager' && $userrole !== 'User'): ?>
-                <button name="Cnew">Create New</button>
-            <?php endif; ?>
+                <button name="Wnew">Create New</button>
             </form>
             <input type="text" id="searchInput" placeholder="Search on the current list..." onkeyup="searchTable()">
         </div>
@@ -84,60 +69,41 @@ $conn->close();
         <div class="table-responsive">
             <table id="myTable" class="table-container" style="width:100%">
                 <thead>
-                    <tr>
-                        <th>WarehouseID</th>
-                        <th>Name</th>
-                        <th>Address</th>
-                        <th>Contact</th>
-                        <th>Email</th>
-                        <?php if ($userrole !== 'User'): ?>
-                        <th>Action</th>
-                        <?php endif; ?>
-                    </tr>
+                <tr>
+                    <th>WarehouseID</th>
+                    <th>Name</th>
+                    <th>Location</th>
+                    <th>Capacity</th>
+                    <th>Action</th>
+                </tr>
                 </thead>
                 <tbody id="tableBody">
-                    <?php if (empty($subsetWarehouses)): ?>
+                <?php if (empty($subsetWarehouses)): ?>
+                    <tr>
+                        <td colspan="5">No data available</td>
+                    </tr>
+                <?php else: ?>
+                    <?php foreach ($subsetWarehouses as $warehouse): ?>
                         <tr>
-                            <td colspan="6">No data available</td>
-                        </tr>
-                    <?php else: ?>
-                        <?php foreach ($subsetWarehouses as $warehouse): ?>
-                            <tr>
-                                <td>
-                                    <?= $warehouse[0] ?>
-                                </td>
-                                <td>
-                                    <?= $warehouse[1] ?>
-                                </td>
-                                <td>
-                                    <?= $warehouse[2] ?>
-                                </td>
-                                <td>
-                                    <?= $warehouse[3] ?>
-                                </td>
-                                <td>
-                                    <?= $warehouse[4] ?>
-                                </td>
-                                <?php if ($userrole !== 'User'): ?>
-                                <td>
-                                    <form method="GET" action="warehouse-edit.php">
-                                        <input type="hidden" name="warehouseID" value="<?= $warehouse[0] ?>">
-                                        <button class="edit" type="submit">edit</button>
-                                    </form>
-                                    <?php if ($userrole !== 'Manager'): ?>
-
-                                    <form method="POST">
-                                        <button class="delete" name="deleteWarehouse" type="submit"
+                            <td><?= $warehouse['WarehouseID'] ?></td>
+                            <td><?= $warehouse['Name'] ?></td>
+                            <td><?= $warehouse['Location'] ?></td>
+                            <td><?= $warehouse['Capacity'] ?></td>
+                            <td>
+                                <form method="GET" action="warehouse-edit.php">
+                                    <input type="hidden" name="warehouseID" value="<?= $warehouse['WarehouseID'] ?>">
+                                    <button class="edit" type="submit">edit</button>
+                                </form>
+                                <form method="POST">
+                                    <button class="delete" name="deleteWarehouse" type="submit"
                                             onclick="return confirm('Are you sure you want to delete this warehouse?')">delete
-                                        </button>
-                                        <input type="hidden" name="deleteWarehouse" value="<?= $warehouse[0] ?>">
-                                    </form>
-                                    <?php endif; ?> 
-                                </td>
-                                <?php endif; ?>   
-                            </tr>
-                        <?php endforeach; ?>
-                    <?php endif; ?>
+                                    </button>
+                                    <input type="hidden" name="deleteWarehouse" value="<?= $warehouse['WarehouseID'] ?>">
+                                </form>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                <?php endif; ?>
                 </tbody>
             </table>
         </div>
@@ -160,8 +126,6 @@ $conn->close();
             <?php endfor; ?>
         </div>
     </main>
-
 </div>
 </body>
-
 </html>
