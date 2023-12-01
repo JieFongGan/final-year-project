e35496060b4d04ca5b920a18474eac726e38dcd1
kahfai0419 <138851255+kahfai0419@users.noreply.github.com>
2023-12-02 00:06:29 +0800

    fix

--- database/database-connect.php
+++ database/database-connect.php
@@ -15,20 +15,11 @@ if (isset($_SESSION['companyname'])) {
 
 
 // Replace these values with your Azure SQL Database connection details
-$serverName = "tcp:allhereserver.database.windows.net,1433";
-$database = "easywire";
-$uid = "sqladmin";
-$pwd = "#Allhere";
+$conn = new PDO(
+    "sqlsrv:server = tcp:allhereserver.database.windows.net,1433; Database = $companyname",
+    "sqladmin",
+    "#Allhere",
+    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+);
 
-// Create a connection
-$conn = new PDO("sqlsrv:server=$serverName;Database=$database", $uid, $pwd);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-// Check the connection
-try {
-    $conn = new PDO("sqlsrv:server=$serverName;Database=$database", $uid, $pwd);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-}
 ?>
