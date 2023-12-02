91870bac311a8511bb490470d27510833dcd8636
jiefong <jiefonggan@gmail.com>
2023-12-02 16:22:31 +0800

    test

--- layout/homepage.php
+++ layout/homepage.php
@@ -1,8 +1,6 @@
 ﻿<?php
 ob_start(); // Start output buffering
-$pageTitle = "Homepage";
 include '../database/database-connect.php';
-include '../contain/header.php';
 
 // Fetch relevant data for the dashboard
 function fetchSingleResult($conn, $query)
@@ -28,141 +26,244 @@ if ($latestTransactionsQuery !== false) {
 }
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<div class="main-content">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
+    <title>
+        home
+    </title>
 
-    <?php
-    $pathtitle = "Homepage";
-    include '../contain/horizontal-bar.php';
-    ?>
+    <!-- styling -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <script src="../script/script.js"></script>
+</head>
 
-    <main>
-        <br>
-
-        <h2 class="dash-title">Overview</h2>
-
-        <div class="dash-cards">
-            <div class="card-single">
-                <div class="card-body">
-                    <span class="ti-briefcase"></span>
-                    <div>
-                        <h5>Total Inventory</h5>
-                        <h4>
-                            <?php echo $totalInventoryResult['totalProducts']; ?>
-                        </h4>
-                    </div>
-                </div>
-                <div class="card-footer">
-                    <a href="../layout/inventory.php">View all</a>
-                </div>
-            </div>
-
-            <div class="card-single">
-                <div class="card-body">
-                    <span class="ti-reload"></span>
-                    <div>
-                        <h5>Pending</h5>
-                        <h4>
-                            <?php echo $pendingTransactionsResult['totalPendingTransactions']; ?> transactions
-                        </h4>
-                    </div>
-                </div>
-                <div class="card-footer">
-                    <a href="../layout/transaction.php">View all</a>
-                </div>
-            </div>
-
-            <div class="card-single">
-                <div class="card-body">
-                    <span class="ti-check-box"></span>
-                    <div>
-                        <h5>Processing/Shipped</h5>
-                        <h4>
-                            <?php echo $processedTransactionsResult['totalProcessedTransactions']; ?> transactions
-                        </h4>
-                    </div>
-                </div>
-                <div class="card-footer">
-                    <a href="../layout/transaction.php">View all</a>
-                </div>
-            </div>
+<body>
+    <input type="checkbox" id="sidebar-toggle">
+    <div class="sidebar">
+        <div class="sidebar-header">
+            <h3 class="brand">
+                <span class="ti-unlink"></span>
+                <span>
+                    <?php echo $companyname ?>
+                </span>
+            </h3>
+            <label for="sidebar-toggle" class="ti-menu-alt"></label>
         </div>
 
-        <section class="recent">
-            <div class="activity-grid">
-                <div class="activity-card">
-                    <h3>Recent activity</h3>
+        <div class="sidebar-menu">
+            <ul>
+                <li>
+                    <a href="../layout/homepage.php">
+                        <span class="ti-home"></span>
+                        <span>Home</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="../layout/inventory.php">
+                        <span class="ti-package"></span>
+                        <span>Inventory</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="../layout/transaction.php">
+                        <span class="ti-shopping-cart"></span>
+                        <span>Transaction</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="../layout/warehouse.php">
+                        <span class="ti-truck"></span>
+                        <span>Warehouse</span>
+                    </a>
+                </li>
+                <?php if ($userrole !== 'User'): ?>
+                    <li>
+                        <a href="../layout/customer.php">
+                            <span class="ti-agenda"></span>
+                            <span>Customer</span>
+                        </a>
+                    </li>
+                <?php endif; ?>
+                <li>
+                    <a href="../layout/report.php">
+                        <span class="ti-pie-chart"></span>
+                        <span>Report</span>
+                    </a>
+                </li>
+                <?php if ($userrole !== 'User'): ?>
+                    <li>
+                        <a href="../layout/settings.php">
+                            <span class="ti-settings"></span>
+                            <span>Setting</span>
+                        </a>
+                    </li>
+                <?php endif; ?>
+            </ul>
+        </div>
+    </div>
+    <div class="main-content">
 
-                    <div class="table-responsive">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Last Updated Date</th>
-                                    <th>Status</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            <?php
-                                foreach ($latestTransactionsResult as $row) {
-                                    $statusClass = ($row['DeliveryStatus'] == 'Pending' || $row['DeliveryStatus'] == 'Processing') ? 'warning' : 'success';
-                                    ?>
-                                    <tr>
-                                        <td><?php echo $row['TransactionType']; ?></td>
-                                        <td><?php echo date('d M, Y', strtotime($row['TransactionDate'])); ?></td>
-                                        <td>
-                                            <span class="badge <?php echo $statusClass; ?>">
-                                                <?php echo $row['DeliveryStatus']; ?>
-                                            </span>
-                                        </td>
-                                    </tr>
-                                <?php
-                                }
-                                ?>
-                            </tbody>
-                        </table>
+        <header>
+            <div class="directory-tag">
+                <p>
+                    home
+                </p>
+            </div>
+
+            <div class="social-icons">
+                <div class="social-icon">
+                    <p style="float: left;
+                margin-right: 8px;
+                margin-top: 8px;">Welcome Back,
+                        <?php echo $username ?>
+                    </p>
+                    <img src="../img/user-profile.png" alt="Social Icon" id="social-icon">
+                    <ul class="dropdown">
+                        <li><a href="../layout/profile.php">Profile</a></li>
+                        <li><a href="../layout/chpassword.php">Change Password</a></li>
+                        <li><a href="../logout.php">log out</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+
+        <main>
+            <br>
+
+            <h2 class="dash-title">Overview</h2>
+
+            <div class="dash-cards">
+                <div class="card-single">
+                    <div class="card-body">
+                        <span class="ti-briefcase"></span>
+                        <div>
+                            <h5>Total Inventory</h5>
+                            <h4>
+                                <?php echo $totalInventoryResult['totalProducts']; ?>
+                            </h4>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <a href="../layout/inventory.php">View all</a>
                     </div>
                 </div>
 
-                <div class="summary">
-                    <div class="summary-card">
-                        <div class="summary-single">
-                            <span class="ti-id-badge"></span>
-                            <div>
-                                <h5>
-                                    <?php echo $userCountResult['userCount']; ?>
-                                </h5>
-                                <small>Number of user</small>
-                            </div>
+                <div class="card-single">
+                    <div class="card-body">
+                        <span class="ti-reload"></span>
+                        <div>
+                            <h5>Pending</h5>
+                            <h4>
+                                <?php echo $pendingTransactionsResult['totalPendingTransactions']; ?> transactions
+                            </h4>
                         </div>
-                        <div class="summary-single">
-                            <span class="ti-calendar"></span>
-                            <div>
-                                <h5>
-                                    <?php echo $warehouseCountResult['warehouseCount']; ?>
-                                </h5>
-                                <small>Number of warehouse</small>
-                            </div>
+                    </div>
+                    <div class="card-footer">
+                        <a href="../layout/transaction.php">View all</a>
+                    </div>
+                </div>
+
+                <div class="card-single">
+                    <div class="card-body">
+                        <span class="ti-check-box"></span>
+                        <div>
+                            <h5>Processing/Shipped</h5>
+                            <h4>
+                                <?php echo $processedTransactionsResult['totalProcessedTransactions']; ?> transactions
+                            </h4>
                         </div>
-                        <div class="summary-single">
-                            <span class="ti-face-smile"></span>
-                            <div>
-                                <h5>
-                                    <?php echo $categoryCountResult['categoryCount']; ?>
-                                </h5>
-                                <small>Category create</small>
-                            </div>
-                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <a href="../layout/transaction.php">View all</a>
                     </div>
                 </div>
             </div>
-        </section>
-    </main>
-</div>
 
-<?php
-ob_end_flush(); // Flush the output buffer and turn off output buffering
-?>
+            <section class="recent">
+                <div class="activity-grid">
+                    <div class="activity-card">
+                        <h3>Recent activity</h3>
+
+                        <div class="table-responsive">
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Last Updated Date</th>
+                                        <th>Status</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    foreach ($latestTransactionsResult as $row) {
+                                        $statusClass = ($row['DeliveryStatus'] == 'Pending' || $row['DeliveryStatus'] == 'Processing') ? 'warning' : 'success';
+                                        ?>
+                                        <tr>
+                                            <td>
+                                                <?php echo $row['TransactionType']; ?>
+                                            </td>
+                                            <td>
+                                                <?php echo date('d M, Y', strtotime($row['TransactionDate'])); ?>
+                                            </td>
+                                            <td>
+                                                <span class="badge <?php echo $statusClass; ?>">
+                                                    <?php echo $row['DeliveryStatus']; ?>
+                                                </span>
+                                            </td>
+                                        </tr>
+                                        <?php
+                                    }
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                    <div class="summary">
+                        <div class="summary-card">
+                            <div class="summary-single">
+                                <span class="ti-id-badge"></span>
+                                <div>
+                                    <h5>
+                                        <?php echo $userCountResult['userCount']; ?>
+                                    </h5>
+                                    <small>Number of user</small>
+                                </div>
+                            </div>
+                            <div class="summary-single">
+                                <span class="ti-calendar"></span>
+                                <div>
+                                    <h5>
+                                        <?php echo $warehouseCountResult['warehouseCount']; ?>
+                                    </h5>
+                                    <small>Number of warehouse</small>
+                                </div>
+                            </div>
+                            <div class="summary-single">
+                                <span class="ti-face-smile"></span>
+                                <div>
+                                    <h5>
+                                        <?php echo $categoryCountResult['categoryCount']; ?>
+                                    </h5>
+                                    <small>Category create</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+    </div>
+
+    <?php
+    ob_end_flush(); // Flush the output buffer and turn off output buffering
+    ?>
 
 </body>
 
