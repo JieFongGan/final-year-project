154d15123fabbc369067b49be10cdd338e60318d
jiefong <jiefonggan@gmail.com>
2023-12-02 16:18:04 +0800

    fix

--- layout/inventory.php
+++ layout/inventory.php
@@ -3,7 +3,6 @@
 ob_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-$pageTitle = "Inventory";
 include '../database/database-connect.php';
 
 // Prepare the SQL statement for inventory
