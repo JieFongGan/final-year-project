410db9daa4dc44c9766bc8bc6ae83280fb85a52e
kahfai0419 <138851255+kahfai0419@users.noreply.github.com>
2023-12-02 10:35:55 +0800

    sql fix

--- admin/adminuserlist.php
+++ admin/adminuserlist.php
@@ -1,196 +1,40 @@
+<?php
+try {
+    $conn = new PDO(
+        "sqlsrv:server=tcp:allhereserver.database.windows.net,1433;Database=allheredb",
+        "sqladmin",
+        "#Allhere",
+        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+    );
+
+    // Fetch data from the database with search conditions
+    $searchKeyword = isset($_GET['search']) ? $_GET['search'] : '';
+    $query = "SELECT CompanyName, UserID, Status FROM user WHERE 
+                CompanyName LIKE :keyword OR
+                UserID LIKE :keyword OR
+                Status LIKE :keyword";
+
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(':keyword', $searchKeyword, PDO::PARAM_STR);
+    $stmt->execute();
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    echo "SQL Query: " . $query; // Add this line for debugging
+    die();
+}
+?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Company List</title>
-    <style>
-        /* CSS beautification */
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            background-color: #f1f1f1;
-        }
-
-        .container {
-            width: 800px;
-            background-color: #fff;
-            padding: 20px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            display: flex;
-        }
-
-        .sidebar {
-            width: 200px;
-            padding: 20px;
-            background-color: #f2f2f2;
-            transition: width 0.3s ease-in-out;
-        }
-
-        .content {
-            flex: 1;
-            padding: 20px;
-        }
-
-        /* Side menu */
-        .sidebar ul {
-            list-style-type: none;
-            padding: 0;
-            margin: 0;
-        }
-
-        .sidebar li {
-            margin-bottom: 10px;
-        }
-
-        .sidebar a {
-            text-decoration: none;
-            color: #333;
-        }
-
-        .sidebar a:hover {
-            color: #666;
-        }
-
-        /* Table styling */
-        table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-
-        th,
-        td {
-            padding: 10px;
-            text-align: left;
-            border-bottom: 1px solid #ddd;
-        }
-
-        th {
-            background-color: #f2f2f2;
-        }
-
-        /* Make table scrollable after 10 entries */
-        .table-container {
-            max-height: 300px;
-            /* Set the maximum height as per your requirement */
-            overflow-y: auto;
-        }
-
-        /* Button styling */
-        button {
-            padding: 10px 20px;
-            background-color: blue;
-            color: #fff;
-            border: none;
-            cursor: pointer;
-            transition: background-color 0.3s ease-in-out;
-        }
-
-        button:hover {
-            background-color: blue;
-        }
-
-        /* Animation */
-        .sidebar:hover {
-            width: 250px;
-        }
-
-        /* Additional CSS */
-        .container {
-            animation: slide-in 0.5s ease-in-out;
-        }
-
-        .pagination {
-            display: flex;
-            list-style: none;
-            margin: 20px 0;
-            padding: 0;
-        }
-
-        .pagination a {
-            display: block;
-            padding: 10px 15px;
-            margin-right: 5px;
-            border: 1px solid #ddd;
-            text-decoration: none;
-            color: #333;
-            background-color: #f2f2f2;
-            border-radius: 4px;
-            transition: background-color 0.3s ease-in-out;
-        }
-
-        .pagination a:hover {
-            background-color: #ddd;
-        }
-
-        @keyframes slide-in {
-            0% {
-                opacity: 0;
-                transform: translateX(-100px);
-            }
-
-            100% {
-                opacity: 1;
-                transform: translateX(0);
-            }
-        }
-
-        .content {
-            animation: fade-in 0.5s ease-in-out;
-        }
-
-        @keyframes fade-in {
-            0% {
-                opacity: 0;
-            }
-
-            100% {
-                opacity: 1;
-            }
-        }
-
-        .search-container {
-            text-align: left;
-        }
-
-        #searchInput {
-            width: 300px;
-            padding: 8px;
-            font-size: 16px;
-        }
-    </style>
+    <!-- Your existing styles go here -->
 </head>
 
 <body>
-    <?php
-    try {
-        $conn = new PDO(
-            "sqlsrv:server = tcp:allhereserver.database.windows.net,1433; Database = allheredb",
-            "sqladmin",
-            "#Allhere",
-            array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
-        );
-
-        // Fetch data from the database with search conditions
-        $searchKeyword = isset($_GET['search']) ? $_GET['search'] : '';
-        $query = "SELECT CompanyName, UserID, Status FROM user WHERE 
-                    CompanyName LIKE :keyword OR
-                    UserID LIKE :keyword OR
-                    Status LIKE :keyword";
-
-        $stmt = $conn->prepare($query);
-        $stmt->bindParam(':keyword', $searchKeyword, PDO::PARAM_STR);
-        $stmt->execute();
-    } catch (PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-        die();
-    }
-    ?>
-
     <div class="sidebar">
         <ul>
             <li><a href="admincomplist.php">Company List</a></li>
@@ -198,9 +42,9 @@
             <li><a href="adlogin.php">Log Out</a></li>
         </ul>
     </div>
+
     <div class="container">
         <div class="content">
-
             <div class="search-container">
                 <form action="adminuserlist.php" method="GET">
                     <input type="text" id="searchInput" name="search" placeholder="Search by Company Name, User ID or Status">
